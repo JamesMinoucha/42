d30b0282dbd4864f40c187960912f439b37ac9bd
James <119378901+JamesMinoucha@users.noreply.github.com>
2023-06-20 12:05:39 +0200

    New One

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-import json
+from rich import print
 import os
 
-modules = [file for file in os.listdir("v2/modules") if file.endswith('.json')]
-for file in modules:   
-    with open('v2/modules/{}'.format(file), "r") as getJson:
-        data = json.load(getJson)
-    print(data["moduleName"])
-print(modules)
+
+##conv = 'conv [file] [to] %'
+bannedFunctionCalls = ["clearTerminal"]
+
+def clearTerminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clearTerminal()
+
+getVaraible = 'getVar [!name]%'
+def getVariable(name):
+    return getattr(globals(), name)
+
+clear = 'clear %'
+def clear():
+    clearTerminal()
+    print("[green]Successfully clear.[/green]")
+
+def commandPrompt():
+    command = input('>> ')
+    commandSplit = command.split()
+
+    if len(command) =< 0:
+        print("NOP")
+    try: 
+        if commandSplit[0] in bannedFunctionCalls:
+            print(f"[red]Commande déjà utiliser par la shell.[/red]")
+        else:
+            print(f"[green]Commande valide.[/green]")
+    except:
+        print(f"[red]Commande invalide.[/red]")
+
+while True:
+    commandPrompt()
+    print("")
